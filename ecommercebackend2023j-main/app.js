cf06394b5ce8cf2121dbe59f97d7734cef0f0996
sh <sandrahammamitlili@gmail.com>
2023-07-29 15:18:41 +0200

    ssl-comit

--- ecommercebackend2023j-main/app.js
+++ ecommercebackend2023j-main/app.js
@@ -5,11 +5,44 @@ const cors=require('cors')
 const categorieRouter =require("./routes/categorie.route")
 const scategorieRouter =require("./routes/scategorie.route")
 const articleRouter=require('./routes/article.route')
+
 dotenv.config()
 const app = express();
 //BodyParser Middleware
 app.use(express.json());
 app.use(cors())
+
+//ssl
+var forceSSL = require('express-force-ssl');
+const https = require('https');
+const fs = require('fs');
+const options = {
+    key: fs.readFileSync('./certification/key.pem'),
+    cert: fs.readFileSync('./certification/cert.pem')
+  };
+
+ app.use(forceSSL); 
+  var secureServer = https.createServer(options, app);
+ // secureServer.listen(443)
+
+ // const server = https.createServer(options, app);  
+
+ // Redirect HTTP to HTTPS
+const httpApp = express();
+httpApp.use((req, res, next) => {
+  if (!req.secure) {
+    return res.redirect(301, `https://${req.hostname}:${process.env.HTTPS_PORT}${req.url}`);
+  }
+  next();
+});
+
+httpApp.listen(process.env.PORT, () => {
+  console.log(`HTTP server listening on port ${process.env.PORT}`);
+});
+
+app.use(forceSSL);
+
+
 mongoose.set("strictQuery", false);
 // Connexion à la base données
 mongoose.connect(process.env.DATABASECLOUD,{
@@ -28,7 +61,9 @@ app.use('/api/categories', categorieRouter);
 app.use('/api/scategories', scategorieRouter);
 app.use('/api/articles',articleRouter);
 
-app.listen(process.env.PORT, () => {
-console.log(`Server is listening on port ${process.env.PORT}`); })
+
+secureServer.listen(process.env.HTTPS_PORT, () => {
+  console.log(`Server is listening on port ${process.env.HTTPS_PORT}`);
+});
 
 module.exports = app;
